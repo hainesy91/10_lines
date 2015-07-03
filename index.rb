142ce5caca3bc1201e62ec54d9acd6b92198e2bc
Christopher Haines <hainesy91@gmail.com>
2015-07-03 12:31:33 +0100

    wrote ruby method for siri easter egg gag

---
+++ index.rb
@@ -0,0 +1,7 @@
+puts "What is 0 divided by 0?"
+answer = gets.to_i
+if answer === 0
+  puts "Imagine that you have zero cookies, and you split them evenly among friends. See? It doesn't make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends."
+else 
+  puts "that is incorrect!"
+end
